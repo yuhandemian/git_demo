5dc07f6c601f05252f6229e2b33658f923d6f950
yuhandemian <yuhn1011@naver.com>
2025-07-04 09:44:45 +0900

    feat: modify Calculator2

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,7 @@
-public class Calculator  {
-    public int add(int a, int b) {
+public class Calculator {
+    public int add(String str1, String str2) {
+        int a = Integer.parseInt(str1);
+        int b = Integer.parseInt(str2);
         return a + b;
     }
 }
