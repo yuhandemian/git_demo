8e3456ab4bdde07cf1c5fb4ceaffcdd84012fd86
yuhandemian <yuhn1011@naver.com>
2025-07-26 15:47:04 +0900

    feat: day11

---
+++ work_space_spring_codeit5/01_spring_strart/src/test/java/com/codeit/start/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
